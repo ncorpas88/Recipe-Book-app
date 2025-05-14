c7cd002d3bff717cc73ebd9a9609c0571c6d5725
Natanael Corpas Rivero <natel1988@gmail.com>
2025-05-14 15:53:44 +0200

    data y estado inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Navbar from "./Components/Navbar.jsx"
 import Footer from "./Components/Footer.jsx"
 import Sidebard from "./Components/Sidebard.jsx"
+import recetasData from "./data/recetas.json"
 import './App.css'
+import { useState } from "react"
 
+const [recetasObj, setRecetasObj] = useState(recetasData)
 
 function App() {
   
