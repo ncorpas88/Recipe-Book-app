eb37c98215c4687468306fa6b8e06ae199170f04
Natanael Corpas Rivero <natel1988@gmail.com>
2025-05-14 15:26:54 +0200

    retoques style

--- src/Components/Sidebard.jsx
+++ src/Components/Sidebard.jsx
@@ -3,9 +3,9 @@ import buttonMenu from "../image/menuu.png";
 function Sidebard() {
   return (
     <section id="contenedor">
-     <div id="button-conten">
+      <div id="button-conten">
         <div id="recetario">
-        <h2>Recetario</h2>
+          <h2>Recetario</h2>
         </div>
         <button id="button">
           <img src={buttonMenu} alt="logo-button" />
