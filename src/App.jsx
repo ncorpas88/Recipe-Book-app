629c1c9e43ad5d28e83dcd356e22860da2a40141
Mackenzie <kiokomachkio@gmail.com>
2025-05-15 16:26:31 +0200

    Reestructuración

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import HomePage from "./Components/Homepage"
+import HomePage from "./pages/Homepage"
 import './App.css'
+import Navbar from "./Components/Navbar";
+import Sidebard from "./Components/Sidebard";
+import Footer from "./Components/Footer";
 
 function App() {
   
 
   return (
     <>
+    <Navbar />
+    <Sidebard/>
+    <Footer />
      <HomePage />
      
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import Navbar from "./Navbar";
-import Sidebard from "./Sidebard";
-import Footer from "./Footer";
 import recetasData from "../data/recetas.json";
 import './Homepage.css';
 import chicote from "../image/chicote.png";
@@ -12,14 +9,13 @@ function HomePage () {
     return (
 
         <>
-        <Navbar />
-        <Sidebard />
+        
             <div className="info-container">
                 <h1 className="titulo">Encuentra tus recetas favoritas</h1>
                 <img className="chicote"  src={chicote} alt="chicotee" />
             
         </div>
-        <Footer />
+        
 
         
         </>
